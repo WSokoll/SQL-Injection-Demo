221ef19d97e3dd1f05580ca5acb9febec14c90cd
WSokoll <wladek.sokol01@gmail.com>
2022-12-12 18:56:21 +0100

    code cleaning

--- app.py
+++ app.py
@@ -82,67 +82,54 @@ def create_app():
     # TEST DATA
     # @app.before_first_request
     # def create_test_data():
-
-        # from models import RolesUsers, Role, User, Account, Currency, SubAccount, InternalTransaction,\
-        #     ExternalTransaction, CurrencyExchange
-
-        # # --------------------------- raw sql to create tables ------------------------------
-        # print(CreateTable(RolesUsers.__table__).compile(dialect=sqlite.dialect()))
-        # print(CreateTable(Role.__table__).compile(dialect=sqlite.dialect()))
-        # print(CreateTable(User.__table__).compile(dialect=sqlite.dialect()))
-        # print(CreateTable(Currency.__table__).compile(dialect=sqlite.dialect()))
-        # print(CreateTable(SubAccount.__table__).compile(dialect=sqlite.dialect()))
-        # print(CreateTable(InternalTransaction.__table__).compile(dialect=sqlite.dialect()))
-        # print(CreateTable(ExternalTransaction.__table__).compile(dialect=sqlite.dialect()))
-        # print(CreateTable(CurrencyExchange.__table__).compile(dialect=sqlite.dialect()))
-        # print(CreateTable(Account.__table__).compile(dialect=sqlite.dialect()))
-        # # -----------------------------------------------------------------------------------
-
-        # # --------------------------- phase one ------------------------------
-        # account1 = Account(active=True, created_at=datetime.datetime.now())
-        # account2 = Account(active=True, created_at=datetime.datetime.now())
-        #
-        # currency1 = Currency(name='pln', exchange_rate=1.0)
-        # currency2 = Currency(name='eur', exchange_rate=4.68)
-        # currency3 = Currency(name='usd', exchange_rate=4.61)
-        #
-        # db.session.add(account1)
-        # db.session.add(account2)
-        # db.session.add(currency1)
-        # db.session.add(currency2)
-        # db.session.add(currency3)
-        # db.session.commit()
-        # # --------------------------------------------------------------------
-
-        # # --------------------------- phase two ------------------------------
-        # sub_account1 = SubAccount(balance=100.1, account_id=1, currency_id=1)
-        # sub_account2 = SubAccount(balance=1000.2, account_id=1, currency_id=2)
-        # sub_account3 = SubAccount(balance=2000.0, account_id=2, currency_id=1)
-        # sub_account4 = SubAccount(balance=300.44, account_id=2, currency_id=2)
-        #
-        # db.session.add(sub_account1)
-        # db.session.add(sub_account2)
-        # db.session.add(sub_account3)
-        # db.session.add(sub_account4)
-        #
-        # user1 = User(email="user@test.com",
-        #              password=sha256_crypt.encrypt("test1"),
-        #              confirmed_at=datetime.datetime.now(),
-        #              account_id=1,
-        #              name='Adam Nowak',
-        #              fs_uniquifier=123)
-        #
-        # user2 = User(email="admin@test.com",
-        #              password=sha256_crypt.encrypt("test2"),
-        #              confirmed_at=datetime.datetime.now(),
-        #              account_id=2,
-        #              name='Marek Kowalski',
-        #              fs_uniquifier=321)
-        #
-        # db.session.add(user1)
-        # db.session.add(user2)
-        #
-        # db.session.commit()
-        # # --------------------------------------------------------------------
+    #
+    #     from models import User, Account, Currency, SubAccount
+    #
+    #     # --------------------------- phase one ------------------------------
+    #     account1 = Account(active=True, created_at=datetime.datetime.now())
+    #     account2 = Account(active=True, created_at=datetime.datetime.now())
+    #
+    #     currency1 = Currency(name='pln', exchange_rate=1.0)
+    #     currency2 = Currency(name='eur', exchange_rate=4.68)
+    #     currency3 = Currency(name='usd', exchange_rate=4.61)
+    #
+    #     db.session.add(account1)
+    #     db.session.add(account2)
+    #     db.session.add(currency1)
+    #     db.session.add(currency2)
+    #     db.session.add(currency3)
+    #     db.session.commit()
+    #     # --------------------------------------------------------------------
+    #
+    #     # --------------------------- phase two ------------------------------
+    #     sub_account1 = SubAccount(balance=100.1, account_id=1, currency_id=1)
+    #     sub_account2 = SubAccount(balance=1000.2, account_id=1, currency_id=2)
+    #     sub_account3 = SubAccount(balance=2000.0, account_id=2, currency_id=1)
+    #     sub_account4 = SubAccount(balance=300.44, account_id=2, currency_id=2)
+    #
+    #     db.session.add(sub_account1)
+    #     db.session.add(sub_account2)
+    #     db.session.add(sub_account3)
+    #     db.session.add(sub_account4)
+    #
+    #     user1 = User(email="user@test.com",
+    #                  password=sha256_crypt.encrypt("test1"),
+    #                  confirmed_at=datetime.datetime.now(),
+    #                  account_id=1,
+    #                  name='Adam Nowak',
+    #                  fs_uniquifier=123)
+    #
+    #     user2 = User(email="admin@test.com",
+    #                  password=sha256_crypt.encrypt("test2"),
+    #                  confirmed_at=datetime.datetime.now(),
+    #                  account_id=2,
+    #                  name='Marek Kowalski',
+    #                  fs_uniquifier=321)
+    #
+    #     db.session.add(user1)
+    #     db.session.add(user2)
+    #
+    #     db.session.commit()
+    #     # --------------------------------------------------------------------
 
     return app
